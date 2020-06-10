f6fae77b1e1c1dc4ce9f7ea80b314a775a295375
Kebalepile <pimpabutterfly86@gmail.com>
2020-06-10 12:10:05 +0200

    store contract

--- migrations/2_deploy_contract.js
+++
@@ -1,9 +0,0 @@
-const TradeStore = artifacts.require("TradeStore");
-// const Sale = artifacts.require("Sale");
-
-
-module.exports =  async function(deployer) {
-  // deply trade store
-   await deployer.deploy(TradeStore);
-  //  await deployer.deploy(Sale);
-};
